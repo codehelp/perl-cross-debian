fc037923d61a7a8a36006285dc6aa9ed2dd658ad
Neil Williams <codehelp@debian.org>
2013-01-11 21:53:49 +0000

    package and update the native script

--- perl-cross-native.sh
+++ perl-cross-native.sh
@@ -2,5 +2,11 @@
 set -e
 set -x
 
+if [ -f config.h ]; then
+	rm config.h
+fi
+if [ -f config.sh ]; then
+	rm config.sh
+fi
 ./Configure -des -Dusedevel 2>&1 | tee ../upstream-native.log
 make uudmap.h
